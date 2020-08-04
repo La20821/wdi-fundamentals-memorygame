077499252e3eda634816b343cbbed84b60937945
Pinder Lauren <PinderLauren@JohnDeere.com>
2020-08-04 12:03:57 -0400

    created game logic

---
+++ memory-game (4)/memory-game/JS/main.js
@@ -0,0 +1,9 @@
+let cardOne ="queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
+console.log("Up and running!");
